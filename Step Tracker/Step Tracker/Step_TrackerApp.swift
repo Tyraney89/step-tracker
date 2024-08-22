eca412c7fe26b50d12d4f416e358abad2526c729
Tyler Burke <124809459+Tyraney89@users.noreply.github.com>
2024-08-22 10:52:50 -0500

    Created Xcode Skeleton Project

---
+++ Step Tracker/Step Tracker/Step_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Step_TrackerApp.swift
+//  Step Tracker
+//
+//  Created by Tyler Burke on 8/22/24.
+//
+
+import SwiftUI
+
+@main
+struct Step_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
